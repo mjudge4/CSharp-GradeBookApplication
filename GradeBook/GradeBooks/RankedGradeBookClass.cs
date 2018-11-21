113d247f6435b7ee13c777e34f07df7b302c882a
mjudge4 <33074714+mjudge4@users.noreply.github.com>
2018-11-21 12:28:37 +0000

    Completed the module"

---
+++ GradeBook/GradeBooks/RankedGradeBookClass.cs
@@ -0,0 +1,15 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBookClass : BaseGradeBook
+    {
+        public RankedGradeBookClass(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
